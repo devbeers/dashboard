f128525dcb0915027dbbafd5cc6942b9dd76d0e2
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2015-11-05 19:20:57 -0600

    Add NPS score and second keen credentials

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,32 +3,96 @@ require('dotenv').load();
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
-  QueryResult = mongoose.model('QueryResult');
-  
-var io;
-  
-function getKeenResults() {
-  var Keen = require('keen-js');
+  QueryResult = mongoose.model('QueryResult'),
+  Keen = require('keen-js'),
+  io;
 
-  var keen_client_surveys = new Keen({
-    projectId: process.env.KEEN_CLIENT_SURVEYS_PROJECT_ID,
-    readKey: process.env.KEEN_CLIENT_SURVEYS_READ_KEY
-  });
+var overallTimeframe = 'this_2_years';
 
-  var npsScoreAverage = new Keen.Query('average', {
-    eventCollection: 'Participants Answers',
-    targetProperty: 'nps_score',
-    timeframe: 'this_2_years'
-  });
+var keen_client_surveys = new Keen({
+  projectId: process.env.KEEN_CLIENT_SURVEYS_PROJECT_ID,
+  readKey: process.env.KEEN_CLIENT_SURVEYS_READ_KEY
+});
 
-  Keen.ready(function() {  
-    keen_client_surveys.run([npsScoreAverage], function(err, res) {
+var keen_participants_list = new Keen({
+  projectId: process.env.KEEN_PARTICIPANTS_LIST_PROJECT_ID,
+  readKey: process.env.KEEN_PARTICIPANTS_LIST_READ_KEY
+});
+
+var npsScoreAverage = { name: 'npsScoreAverage' };
+npsScoreAverage.queries = [new Keen.Query('average', {
+  eventCollection: 'Participants Answers',
+  targetProperty: 'nps_score',
+  timeframe: overallTimeframe
+})];
+npsScoreAverage.callback = function(err, res) {
+  if (err) throw('error charting: ' + err);
+  else {
+    var queryResult = res.result;
+    
+    saveAndEmitQuery(npsScoreAverage.name, queryResult);
+  }
+};
+
+// Calculate NPS score
+var npsScoreCount = new Keen.Query('count', {
+  eventCollection: 'Participants Answers',
+  targetProperty: 'nps_score',
+  timeframe: overallTimeframe
+});
+
+var npsScoreDetractors = new Keen.Query('count', {
+  eventCollection: 'Participants Answers',
+  targetProperty: 'nps_score',
+  timeframe: overallTimeframe,
+  filters: [
+          {
+            "property_name" : "nps_score",
+            "operator" : "lt",
+            "property_value" : 7
+          }
+       ]
+});
+
+var npsScorePromoters = new Keen.Query('count', {
+  eventCollection: 'Participants Answers',
+  targetProperty: 'nps_score',
+  timeframe: overallTimeframe,
+  filters: [
+          {
+            "property_name" : "nps_score",
+            "operator" : "gt",
+            "property_value" : 8
+          }
+       ]
+});
+
+var npsScoreQuery = { name: 'npsScoreParticipants' };
+npsScoreQuery.queries = [npsScoreCount, npsScoreDetractors, npsScorePromoters];
+npsScoreQuery.callback = function(err, res) {
+  if (err) throw('error charting: ' + err);
+  else {
+    var queryResult = ((res[2].result - res[1].result) / res[0].result).toFixed(2) * 100;
+    
+    saveAndEmitQuery(npsScoreQuery.name, queryResult);
+  }
+};
+
+function saveAndEmitQuery(queryName, queryResult) {
+  var query = new QueryResult();
+  query.name = queryName;
+  query.result = queryResult;
+  query.save(function (err) {
+    if (err) throw('error saving: ' + err);
+    else {
       io.on('connection', function(socket) {
-        io.emit('message', res);
+        io.emit('message', query);
       });
-    });
+    }
   });
 }
+
+var keenQueries = [npsScoreAverage, npsScoreQuery];
 
 module.exports = function (app, server) {
   io = require('socket.io')(server);
@@ -36,8 +100,21 @@ module.exports = function (app, server) {
 };
 
 router.get('/', function (req, res, next) {  
-  getKeenResults();
-  
+  keenQueries.forEach(function(query) {
+    QueryResult.findOne({ 'name': query.name }, function(err, queryResult) {
+      if (err) throw('error saving: ' + err);
+      if (!queryResult) {
+        Keen.ready(function() {  
+          keen_client_surveys.run(query.queries, query.callback);
+        });
+      } else {
+        io.on('connection', function(socket) {
+          io.emit('message', queryResult);
+        });
+      }
+    });
+  });
+    
   res.render('index', {
     title: 'devbeers Dashboard'
   });
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,19 +3,29 @@ socket.on('message', function(msg) {
   drawObj(msg);
 });
 
-function drawObj(queryResult) {
-  Keen.ready(function() {  
-    var npsScoreAverageChart = new Keen.Dataviz()
-      .el(document.getElementById('nps-score-participants-average'))
-      .chartType('metric')
-      .title('NPS Score Average - Participants')
-      .colors(['#49c5b1'])
-      .prepare();
+var npsScoreAverageChart = new Keen.Dataviz()
+  .el(document.getElementById('nps-score-participants-average'))
+  .chartType('metric')
+  .colors(['#49c5b1'])
+  .title('NPS Score Average')
+  .prepare();
+  
+var npsScoreParticipantsChart = new Keen.Dataviz()
+  .el(document.getElementById('nps-score-participants'))  
+  .chartType('metric')
+  .title('NPS Score')
+  .colors(['#49c5b1'])
+  .prepare();
+  
+var keenObjs = {};
 
-    npsScoreAverageChart
-      .parseRawData({
-        result: queryResult.result
-      })
-      .render();
-  });
+keenObjs.npsScoreAverageChart = npsScoreAverageChart;
+keenObjs.npsScoreParticipantsChart = npsScoreParticipantsChart;
+
+function drawObj(queryResult) {
+  keenObjs[queryResult.name + 'Chart']
+    .parseRawData({
+      result: queryResult.result
+    })
+    .render();
 }
